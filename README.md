216fd24d1ddcf08d0f80c47d44d70bf12ef04109
DmitriyR54 <45ryirtimd@gmail.com>
2022-05-24 20:44:11 +0300

    bday commit *_* (animated logo)

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Website of the design studio. Portfolio project.
 Used libraries and services:
 
 1. SVG icons - https://icons8.com/icon
+2. * svg generator - https://boxy-svg.com/
--- brume-interior/src/sections/TestSection/TestComponent.js
+++ brume-interior/src/sections/TestSection/TestComponent.js
@@ -3,6 +3,16 @@ import './TestComponent.scss';
 
 const TextComponent = () => {
     document.body.innerHTML += testHtml;
+
+    const canvas = document.querySelector('.logo');
+    console.log(canvas);
+    const ctx = canvas.getContext('2d');
+    
+    ctx.beginPath();
+    ctx.moveTo(100, 50);
+    ctx.lineTo(150, 50);
+    ctx.lineWidth = '5';
+    ctx.stroke();
 };
 
 export default TextComponent;
