c64ee17eb8bfa946799ca630aba7aa6e02c3d530
DmitriyR54 <45ryirtimd@gmail.com>
2022-07-02 22:56:57 +0300

    form section done

--- brume-interior/src/sections/FormSection/FormSection.js
+++ brume-interior/src/sections/FormSection/FormSection.js
@@ -1,8 +1,32 @@
 import formHtml from './FormSection.html';
 import './FormSection.scss';
 
+import { openModal, closeModal } from 'Components/ModalWindow/Modal';
+
 const FormSection = (container) => {
     container.innerHTML += formHtml;
+
+    window.addEventListener('DOMContentLoaded', () => {
+        const form = document.querySelector('.form');
+        const formModal = document.querySelector('.form__modal');
+
+        const submitForm = (event) => {
+            const name = form.querySelector('.form__input-name').value;
+            const nameWrapper = document.querySelector('.form__modal-text-name');
+            nameWrapper.innerHTML = name;
+            event.preventDefault();
+            openModal(formModal);
+        };
+
+        form.onsubmit = submitForm;
+
+        formModal.addEventListener('click', () => closeModal(formModal));
+        formModal.addEventListener('keydown', (event) => {
+            if (event.key === 'Escape') {
+                closeModal(formModal);
+            }
+        });
+    });
 };
 
 export { FormSection };
